0c1417abf6731402c5c1020073f0810a76eaa054
Chotika T <54322772+G-nap@users.noreply.github.com>
2023-03-01 01:02:09 +0700

    update frontend

--- packages/frontend/src/Components/subPlan/subPlan.jsx
+++ packages/frontend/src/Components/subPlan/subPlan.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import "./subPlan.css";
 import Bizbtn from "../bizbtn/bizbtn";
+import { BsFillCheckSquareFill } from "react-icons/bs";
 
 function subPlan() {
   return (
     <div>
-      <div className="sub                                                                                                                                                                                                           -card">
-        <div className="d-flex ">
-          <div className="sub-card-logo"> </div>
-          <div>Plan</div>
-        </div>
+      <div className="sub">
         <div className="sub-card">
-          <div>$ 0</div>
-          <div> / investment project</div>
-          <div> / investment project</div>
-          <div> / investment project</div>
-          <div> / investment project</div>
-        </div>
 
-        <Bizbtn name="Start trial" />
+          <div className="d-flex ">
+            <div className="sub-card-logo"> </div>
+            <div className="sub-card-txt-h1">Plan</div>
+          </div>
+          <div className="sub-card-txt-price">$ 0</div>
+          <div className="d-flex mb-2">
+            <div className="d-flex flex-column sub-card-icon">
+              <div className="mb-1"><BsFillCheckSquareFill /></div>
+              <div className="mb-1"><BsFillCheckSquareFill /></div>
+              <div className="mb-1"><BsFillCheckSquareFill /></div>
+              <div className="mb-1"><BsFillCheckSquareFill /></div>
+         
+            </div>
+            <div>
+              <div className="sub-card-txt-p mb-1">Investment Project</div>
+              <div className="sub-card-txt-p mb-1">Unlimited Project</div>
+              <div className="sub-card-txt-p mb-1">Financial Feasibility Canvas</div>
+              <div className="sub-card-txt-p mb-1">Compare Project</div>
+              </div>
+          </div>
+
+          <Bizbtn name="Start trial" />
+        </div>
       </div>
     </div>
   );
--- packages/frontend/src/Components/subscriptionUserBanner/subscriptionUserBanner.jsx
+++ packages/frontend/src/Components/subscriptionUserBanner/subscriptionUserBanner.jsx
@@ -6,22 +6,22 @@ function subscriptionUserBanner() {
   return (
     <div className="subscript-banner">
       <div className="t-box">
-        <div>Your Subscription Plan</div>
-        <div>Free trial</div>
+        <div className="sub-banner-text-p mb-1">Your Subscription Plan</div>
+        <div className="sub-banner-text-h2">Free trial</div>
       </div>
       <div className="t-box2">
         <div>
-          <div>Payment</div>
+          <div className="sub-banner-text-h3 mb-3">Payment</div>
           <div className="master-card-text">
             <div>
               <div>
-                <SiMastercard />
-                Mastercard ending in 7317
+                <SiMastercard /> 
+                &nbsp;Mastercard ending in 7317
               </div>
               <div>Expires: 03/2026</div>
             </div>
           </div>
-          <div>Next Billing is on 10 Oct 2022</div>
+          <div className="mb-2">Next Billing is on 10 Oct 2022</div>
           <a href="">
             <div>Update</div>
           </a>
